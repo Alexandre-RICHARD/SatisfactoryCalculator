359729e5150efb0a43b4e01d4cdb590d0f306b4f
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-10-26 19:02:08 +0200

    Extract Graph buttons in another components

--- src/react/components/GraphContainer/index.tsx
+++ src/react/components/GraphContainer/index.tsx
@@ -1,16 +1,14 @@
-import { IconContainer, IconTokenEnum } from "@nexus/nexusExporter";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { type Data, Network } from "vis-network";
 import { useShallow } from "zustand/react/shallow";
 
 import { GraphFactoryLineDirectionEnum as GraphDirection } from "../../../enums/graphFactoryLineDirection.enum";
 import { GraphFactoryLineSizeEnum as GraphSize } from "../../../enums/graphFactoryLineSize.enum";
-import { stringRemoveEndPxHelper } from "../../../helpers/stringRemoveEndPx.helper";
 import { useCombinedStore } from "../../../store/combined.store";
-import { theme } from "../../../styles/theme";
 import { useCalculateFactoryLine } from "../../hooks/useCalculateFactoryLine";
 import { useGetDiagramData } from "../../hooks/useGetDiagramData";
 import { useGetDiagramOptions } from "../../hooks/useGetDiagramOptions";
+import { GraphHandlerButtons } from "./GraphHandlerButtons";
 import styles from "./styles.module.scss";
 
 export const GraphContainer = (): React.JSX.Element | null => {
@@ -63,63 +61,12 @@ export const GraphContainer = (): React.JSX.Element | null => {
       ref={graphContainerRef}
     >
       <div ref={visJsRef} />
-      {/* TODO T => Sortir les boutons d'ici */}
-      <div className={styles.graph_button_container}>
-        <button
-          aria-label="fullscreen graph button"
-          type="button"
-          onClick={() => {
-            setGraphSize(
-              graphSize === GraphSize.NORMAL
-                ? GraphSize.FULLSCREEN
-                : GraphSize.NORMAL,
-            );
-          }}
-        >
-          <IconContainer
-            size={2 * stringRemoveEndPxHelper(theme.spaceXL)}
-            iconToken={
-              graphSize === GraphSize.FULLSCREEN
-                ? IconTokenEnum.Collapse
-                : IconTokenEnum.Expand
-            }
-            color={theme.colorSecondaryContent}
-          />
-        </button>
-        <button
-          aria-label="fullscreen graph button"
-          type="button"
-          onClick={() => {
-            switch (graphDirection) {
-              case GraphDirection.LEFT_TO_RIGHT:
-                setGraphDirection(GraphDirection.TOP_TO_BOTTOM);
-                break;
-              case GraphDirection.TOP_TO_BOTTOM:
-                setGraphDirection(GraphDirection.RIGHT_TO_LEFT);
-                break;
-              case GraphDirection.RIGHT_TO_LEFT:
-                setGraphDirection(GraphDirection.BOTTOM_TO_TOP);
-                break;
-              case GraphDirection.BOTTOM_TO_TOP:
-                setGraphDirection(GraphDirection.LEFT_TO_RIGHT);
-                break;
-              default:
-                break;
-            }
-          }}
-        >
-          <div
-            className={styles.arrow_button_rotator_container}
-            data-direction={graphDirection}
-          >
-            <IconContainer
-              size={2 * stringRemoveEndPxHelper(theme.spaceXL)}
-              iconToken={IconTokenEnum.Arrow}
-              color={theme.colorSecondaryContent}
-            />
-          </div>
-        </button>
-      </div>
+      <GraphHandlerButtons
+        graphSize={graphSize}
+        setGraphSize={setGraphSize}
+        graphDirection={graphDirection}
+        setGraphDirection={setGraphDirection}
+      />
     </div>
   );
 };
