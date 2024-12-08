a61470dd50ec485e014e86a97c4f11115de8fb19
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-12-08 17:31:30 +0100

    Upgrade build folder organization

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,36 @@ export default defineConfig(({ mode }) => {
       cssCodeSplit: false,
       manifest: true,
       outDir: "./build",
-      rollupOptions: { input: { app: "./index.html" } },
+      rollupOptions: {
+        input: { app: "./index.html" },
+        output: {
+          assetFileNames: (assetInfo) => {
+            const fileName = assetInfo.names?.[0];
+            if (fileName) {
+              const extType = fileName.split(".").pop();
+              if (extType === "png") {
+                return "assets/images/[name]-[hash][extname]";
+              }
+              if (extType === "ico") {
+                return "assets/[name][extname]";
+              }
+              return "[name]-[hash][extname]";
+            }
+            return "assets/others/[name]-[hash][extname]";
+          },
+          manualChunks: (fileName) => {
+            if (fileName.includes("node_modules")) return "nodeModules";
+            const translationsFilesRegex =
+              /src\/.*\/translations\/.*\.translations\.ts/;
+            if (translationsFilesRegex.test(fileName)) {
+              const language = fileName.split("translations/")[1].split("/")[0];
+              return `translations-${language}`;
+            }
+            if (fileName.includes("/src/")) return "satisfactoryProject";
+            return "app";
+          },
+        },
+      },
     },
     test: {
       environment: "jsdom",
