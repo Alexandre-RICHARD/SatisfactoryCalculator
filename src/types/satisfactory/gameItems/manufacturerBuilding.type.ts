08d60425464bc836e0e3c7cf899ee6f9ab44ced6
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-12-10 10:58:56 +0100

    Change gameItemDictionnary keys from className to just name

--- src/types/satisfactory/gameItems/manufacturerBuilding.type.ts
+++ src/types/satisfactory/gameItems/manufacturerBuilding.type.ts
@@ -1,9 +1,11 @@
 import type { EndpointEnum } from "../../../enums/endpoint.enum";
 import type { GameBuildingsTypeEnum } from "../../../enums/gameBuildingsType.enum";
+import type { GameClassNamesEnum } from "../../../enums/gameClassNames.enum";
 import type { GameItemsEnum } from "../../../enums/gameItems.enum";
 import type { GameItemsCategoryEnum } from "../../../enums/gameItemsCategory.enum";
 
 export type GameItemManufacturerBuilding = {
+  className: GameClassNamesEnum;
   name: GameItemsEnum;
   category: GameItemsCategoryEnum.Building;
   buildingType: GameBuildingsTypeEnum.Manufacturer;
--- src/types/satisfactory/gameItems/resource.type.ts
+++ src/types/satisfactory/gameItems/resource.type.ts
@@ -1,8 +1,10 @@
+import type { GameClassNamesEnum } from "../../../enums/gameClassNames.enum";
 import type { GameItemsEnum } from "../../../enums/gameItems.enum";
 import type { GameItemsCategoryEnum } from "../../../enums/gameItemsCategory.enum";
 import type { GameResourcesTypeEnum } from "../../../enums/gameResourcesType.enum";
 
 export type GameItemResource = {
+  className: GameClassNamesEnum;
   name: GameItemsEnum;
   category: GameItemsCategoryEnum.Resource;
   resourceType: GameResourcesTypeEnum;
