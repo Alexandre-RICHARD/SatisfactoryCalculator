42c4f244865b854ebcee825d6623b5dd0fa0cdc6
Alexandre Richard <alexandre.richard.dev@gmail.com>
2024-12-10 13:52:37 +0100

    Remove all olf items related things (commit not working)

--- src/enums/powerBuildings.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum PowerBuildingsEnum {
-  Biomass_Burner = "Biomass_Burner",
-  Coal_Generator = "Coal_Generator",
-  // TODO A =>
-  // Fuel_Generator = "Fuel_Generator",
-  // Geothermal_Generator = "Geothermal_Generator",
-  // Nuclear_Power_Plant = "Nuclear_Power_Plant",
-}
--- src/types/satisfactory/items.type.ts
+++
@@ -1,7 +0,0 @@
-import type { ItemsEnum } from "../../enums/items.enum";
-
-export type ItemsType = {
-  name: ItemsEnum;
-  isRaw?: true;
-  isLiquid?: true;
-};
